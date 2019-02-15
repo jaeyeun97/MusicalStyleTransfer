fd9e74230db47ee3c8ec1f0720481acca7248a41
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2019-02-15 20:44:49 +0000

    Finished dataset definition

--- data/youtube_dataset.py
+++ data/youtube_dataset.py
@@ -14,8 +14,12 @@ You need to implement the following functions:
 """
 from data.base_dataset import BaseDataset
 from data.audio_folder import make_dataset, default_loader
+from util import mkdir
 
+import csv
+import os
 import librosa
+import soundfile as sf
 import torch
 
 
@@ -32,9 +36,10 @@ class YoutubeDataset(BaseDataset):
         Returns:
         the modified parser.
         """
-        parser.add_argument('--hop_length', type=float, default=30.0, help='Stride in reading in audio')
-        parser.add_argument('--frame_length', type=float, default=60.0, help='Length of each audio sample when processing')
-        parser.add_argument('--stft-binsize', type=int, default=2048, help='Number of Frequency bins for STFT')
+        parser.add_argument('--stride', type=int, default=15, help='Stride in reading in audio (in secs)')
+        parser.add_argument('--length', type=int, default=30, help='Length of each audio sample when processing (in secs)')
+        parser.add_argument('--sample_rate', type=int, default=22050, help='Sample Rate to resample')
+        parser.add_argument('--nfft', type=int, default=2048, help='Number of Frequency bins for STFT')
         parser.add_argument('--mel', type=bool, default=False, help='Use the mel scale')
         parser.add_argument('--subdir', type=str, default="splits", help='Subdir of audio data to use')
         parser.set_defaults(max_dataset_size=10, new_dataset_option=2.0)  # specify dataset-specific default values
@@ -54,37 +59,81 @@ class YoutubeDataset(BaseDataset):
         # save the option and dataset root
         BaseDataset.__init__(self, opt)
         # get the audio path
-        dir = os.path.isdir('{}/{}', self.root, opt.subdir)
-        self.audio_paths = sorted(make_dataset(dir, opt.max_dataset_size))
-        # load audio, pad, split, store it
+        self.dirpath = os.path.isdir('{}/{}', self.root, opt.subdir)
+
+        # TODO: Use cache if it exists
+        # self.cachedir = os.path.join(self.dirpath, '__cache')
+        # mkdir(self.cachedir)
+        # metadata_file = os.path.join(self.cachedir, 'metadata.csv')
+        # if os.path.exist(metadata_file):
+        #     with open(metadata_file, 'r') as f:
+        #         metadata = json.load(f)
+        # else:
+        #     metadata = dict()
+
+        self.audio_paths = sorted(make_dataset(self.dirpath, opt.max_dataset_size))
+        self.frame_paths = list()
+        self.sample_rate = opt.sample_rate
+        self.length = opt.length
+        self.stride = opt.stride
+        self.nfft = opt.nfft
+        self.subdir = opt.subdir
+        self.mel = opt.mel
+
         for p in self.audio_paths:
-            pass
-        # otherwise, split the audio to set size and store
-        # define the default transform function. You can use <base_dataset.get_transform>; You can also define your custom transform function
-        # self.transform = get_transform(opt)
+            # Load audio
+            filename = os.path.splitext(os.path.basename(p))[0]
+            y, sr = sf.read(p, dtype='float32')
+            t = librosa.get_duration(y=y, sr=sr)
+            # Resample Audio
+            if sr != self.sample_rate:
+                y = librosa.resample(y, sr, self.sample_rate)
+            # Pad the audio
+            l = t % opt.stride
+            if l != 0:
+                t = t + 15 - l
+                y = librosa.util.fix_length(y, t * self.sample_rate)
+            # Length Check
+            if t < self.length:
+                print('Skipping {} due to short content length'.format(p))
+                continue
+            # Compute frames and store
+            frames = self.frame(y, sr, length=self.length, stride=self.stride)
+            # cannot store all the frames; store it into files
+            for i in len(frames):
+                fp = os.path.join(cachedir, '{}.{}.npy'.format(filename, i))
+                np.save(fp, frames[:, i])
+                self.frame_paths.append({
+                    'file': filename,
+                    'frame': i,
+                    'path': fp
+                })
 
     def __getitem__(self, index):
         """Return a data point and its metadata information.
 
         Parameters:
         index -- a random integer for data indexing
-
-        Returns:
-        a dictionary of data with their names. It usually contains the data itself and its metadata information.
-
-        Step 1: get a random image path: e.g., path = self.image_paths[index]
-        Step 2: load your data from the disk: e.g., image = Image.open(path).convert('RGB').
-        Step 3: convert your data to a PyTorch tensor. You can use helpder functions such as self.transform. e.g., data = self.transform(image)
-        Step 4: return a data point as a dictionary.
         """
-        path = self.audio_paths[index]
-        y, sr = librosa.load(path)
-        return {'data_A': y, 'A_sr': sr, 'A_path': path}
+        metadata = self.frame_paths[index]
+        frame = np.load(metadata['path'])
+        data = self.transform(frame)
+        result = {'data': data}
+        result.update(metadata)
+        return result
 
     def __len__(self):
         """Return the total number of images."""
-        return len(self.audio_paths)
+        return len(self.frame_paths)
 
+    def transform(self, frame):
+        if self.mel:
+            frame = hz_to_mel(frame)
+        # STFT
+        D = librosa_stft(frame, nfft=self.nfft)
+        lmag, agl = librosa_calc(D)
+        # TODO: add normalization
+        return combine_mag_angle(lmag, agl)
 
     def librosa_stft(y, nfft=2048):
         return librosa.stft(y, nfft=nfft)
@@ -93,11 +142,17 @@ class YoutubeDataset(BaseDataset):
         return torch.stft(x, nfft=nfft)
 
     def librosa_calc(D):
-        return np.array([(np.abs(D[i][j]), np.angle(D[i][j])) for i in range(len(D)) for j in range(len(D[i]))])
+        log_mag = np.log(np.abs(D))
+        agl = np.angle(D)
+        return torch.from_numpy(log_mag), torch.from_numpy(agl)
 
-    def torch_calc(x):
+    def torch_calc(D):
+        x = torch.from_numpy(D)
         real = x[:, : , :, 0]
         comp = x[:, : , :, 1]
-        mag = torch.sqrt(torch.add(torch.pow(real, 2), torch.pow(comp, 2)))
+        log_mag = torch.sqrt(2 * torch.log(real) + 2 * torch.log(comp))
         agl = torch.atan(torch.div(comp, real))
+        return log_mag, agl
+
+    def combine_mag_angle(mag, agl):
         return torch.stack((mag, agl), 2)
