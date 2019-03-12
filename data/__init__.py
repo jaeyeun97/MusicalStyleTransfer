8c3f43ae781814b277d5646bca94b7b4fe841bb9
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2019-03-12 14:38:01 +0000

    dataloader fix

--- data/__init__.py
+++ data/__init__.py
@@ -80,45 +80,63 @@ def create_dataset(opt):
 class DatasetLoader():
     """Wrapper class of Dataset class that performs multi-threaded data loading"""
 
-    def __init__(self, opt, prefix):
+    def __init__(self, opt):
         """Initialize this class
 
         Step 1: create a dataset instance given the name [dataset_mode]
         Step 2: create a multi-threaded data loader.
         """
         self.opt = opt
-        dataset_class = find_dataset_using_name(getattr(opt, '{}_dataset'.format(prefix)))
-        if prefix == 'pair':
-            self.dataset = dataset_class(opt)
-            self.max_size = self.opt.max_dataset_size
+        if opt.single:
+            A_class = find_dataset_using_name(getattr(opt, 'A_dataset'))
+            A_dataset = A_class(opt, 'A') 
+            self.A_dataset = self.get_dataloader(A_dataset)
+            B_class = find_dataset_using_name(getattr(opt, 'B_dataset'))
+            B_dataset = B_class(opt, 'B')
+            self.B_dataset = self.get_dataloader(B_dataset)
+            self.size = max(len(A_dataset), len(B_dataset))
+            print("Single dataset [{}] and [{}] was created".format(
+                type(A_dataset).__name__,
+                type(B_dataset).__name__))
         else:
-            self.dataset = dataset_class(opt, prefix) 
-            self.max_size = getattr(opt, '{}_max_dataset_size'.format(prefix))
-        print("dataset [%s] was created" % type(self.dataset).__name__)
-            
-        self.dataloader = torch.utils.data.DataLoader(
-            self.dataset,
-            batch_size=opt.batch_size,
-            shuffle=not opt.serial_batches,
-            num_workers=int(opt.num_threads))
+            dataset_class = find_dataset_using_name(getattr(opt, 'pair_dataset'))
+            dataset = dataset_class(opt) 
+            self.size = len(dataset) 
+            self.dataset = self.get_dataloader(dataset)
+            print("Pair dataset [%s] was created" % type(dataset).__name__)
+
+    def get_dataloader(self, dataset): 
+        return torch.utils.data.DataLoader(
+            dataset,
+            batch_size=self.opt.batch_size,
+            shuffle=not self.opt.serial_batches,
+            num_workers=int(self.opt.num_threads))
 
     def __len__(self):
         """Return the number of data in the dataset"""
-        return min(len(self.dataset), self.max_size)
+        return self.size 
 
-    def __iter__(self):
-        """Return a batch of data"""
+    def get_iter(self, dataloader):
         i = 0
-        it = iter(self.dataloader)
-        while i * self.opt.batch_size < self.max_size:
+        it = iter(dataloader)
+        while i * self.opt.batch_size < self.size:
             try:
                 yield next(it)
                 i += 1
             except ValueError as e:
                 print(e)
-                continue
-            except audioread.NoBackendError:
-                print('Error Loading Data')
-                continue 
+            except audioread.NoBackendError as e:
+                print(e)
             except StopIteration:
-                break
+                it = iter(dataloader) 
+
+    def __iter__(self):
+        """Return a batch of data"""
+        if self.opt.single:
+            A_iter = self.get_iter(self.A_dataset)
+            B_iter = self.get_iter(self.B_dataset)
+            dataset = zip(A_iter, B_iter)
+        else:
+            dataset = self.get_iter(self.dataset)
+        return dataset
+
