0b46a2647c65555896ce0e08dd4c299649a546a7
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2019-03-20 19:21:38 +0000

    GPU saving

--- styletransfer/models/networks/generator.py
+++ styletransfer/models/networks/generator.py
@@ -5,20 +5,32 @@ from .util import get_norm_layer, init_weights
 
 
 def getGenerator(device, opt):
-    args = dict(opt.__dict__)
-    encoding_model = opt.autoencoder
-    transformer_model = opt.transformer
-    args['norm_layer'] = get_norm_layer(opt.norm_layer)
+    return Generator(opt).to(device)
 
-    if transformer_model == 'resnet':
-        args['transformer'] = Resnet(**args)
-    else:
-        args['transformer'] = None
 
-    if encoding_model == 'conv':
-        netG = ConvAutoencoder(**args)
-    else:
-        raise NotImplementedError('Encoding Model not implemented') 
+class Generator(nn.Module):
+    def __init__(self, opt):
+        super(Generator, self).__init__()
+        args = dict(opt.__dict__)
+        encoding_model = opt.autoencoder
+        transformer_model = opt.transformer
+        args['norm_layer'] = get_norm_layer(opt.norm_layer)
+        self.device = None
 
-    init_weights(netG, opt.init_type, opt.init_gain)
-    return netG.to(device)
+        if transformer_model == 'resnet':
+            args['transformer'] = Resnet(**args)
+        else:
+            args['transformer'] = None
+
+        if encoding_model == 'conv':
+            self.net = ConvAutoencoder(**args)
+        else:
+            raise NotImplementedError('Encoding Model not implemented') 
+        init_weights(self.net, opt.init_type, opt.init_gain)
+
+    def forward(self, i):
+        return self.net(i)
+
+    def to(self, device):
+        self.device = device
+        return super(Generator, self).to(device)
